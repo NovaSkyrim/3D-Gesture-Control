17033c15b373478e76730654a892487fd5582357
Paquito35 <162102769+Paquito35@users.noreply.github.com>
2024-06-03 19:43:32 +0200

    Update app.py
    ajout des commentaire pour la partie manipulation 3d

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+from objloader import *
 import csv
 import copy
 import argparse
@@ -55,73 +56,6 @@ def get_args():
     return args
 
 
-class OBJ:
-    def __init__(self, filename, swapyz=False):
-        """Loads a Wavefront OBJ file. """
-        self.vertices = []
-        self.normals = []
-        self.faces = []
-
-        for line in open(filename, "r"):
-            if line.startswith('#'): continue
-            values = line.split()
-            if not values: continue
-            if values[0] == 'v':
-                v = list(map(float, values[1:4]))
-                if swapyz:
-                    v = v[0], v[2], v[1]
-                self.vertices.append(v)
-            elif values[0] == 'vn':
-                v = list(map(float, values[1:4]))
-                if swapyz:
-                    v = v[0], v[2], v[1]
-                self.normals.append(v)
-            elif values[0] == 'f':
-                face = []
-                norms = []
-                for v in values[1:]:
-                    w = v.split('/')
-                    face.append(int(w[0]))
-                    if len(w) >= 3 and len(w[2]) > 0:
-                        norms.append(int(w[2]))
-                    else:
-                        norms.append(0)
-                self.faces.append((face, norms))
-
-        self.gl_list = glGenLists(1)
-        glNewList(self.gl_list, GL_COMPILE)
-        glFrontFace(GL_CCW)
-        for face in self.faces:
-            vertices, normals = face
-
-            glBegin(GL_POLYGON)
-            for i in range(len(vertices)):
-                if normals[i] > 0:
-                    glNormal3fv(self.normals[normals[i] - 1])
-                glVertex3fv(self.vertices[vertices[i] - 1])
-            glEnd()
-        glEndList()
-
-
-class Camera:
-    def __init__(self, position, target, up):
-        self.position = position
-        self.target = target
-        self.up = up
-        self.zoom_factor = 1.0
-
-    def zoom(self, factor):
-        self.zoom_factor *= factor
-        self.position = [pos * factor for pos in self.position]
-        self.target = [tar * factor for tar in self.target]
-        self.up = [up * factor for up in self.up]
-        self.update_view_matrix()
-
-    def update_view_matrix(self):
-        glLoadIdentity()
-        gluLookAt(*self.position, *self.target, *self.up)
-
-
 pygame.init() # Initialise le module Pygame
 viewport = (1200, 900) # Définir la taille de la fenêtre
 
@@ -149,7 +83,7 @@ obj = OBJ(file_path, swapyz=True)
 clock = pygame.time.Clock()
 
 glMatrixMode(GL_PROJECTION) # Définir la matrice de projection
-glLoadIdentity() # Réinitialiser la matrice de projection
+glLoadIdentity()# Réinitialiser la matrice de projection
 
 # Définir la perspective avec un champ de vision de 90 degrés, un ratio largeur/hauteur, et des plans de clipping proches et lointains
 width, height = viewport
@@ -208,9 +142,9 @@ def main():
 
     mode = 0
 
-    rx, ry, rz = (0, 0, 0)
-    tx, ty = (0, 0)
-    zpos = 10
+    rx, ry, rz = (0, 0, 90)
+    tx, ty, tz = (-3000, 0, 0)
+    zpos = 600
 
     counter_translation = 0
     counter_reset = 0
@@ -219,16 +153,14 @@ def main():
     while True:
 
         clock.tick(60) # Limiter la boucle à 60 images par seconde
-        
         for e in pygame.event.get():
             if e.type == QUIT:
                 sys.exit()
             elif e.type == KEYDOWN and e.key == K_ESCAPE:
                 sys.exit()
-        
+
         # Effacer les buffers de couleur et de profondeur pour préparer la nouvelle image
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        
         glLoadIdentity() # Réinitialiser la matrice modèle/vue
 
         # RENDER OBJECT
